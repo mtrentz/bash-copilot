6cb6bba929bb076bea191cca0f22e3cd0ea40239
mtrentz <mateus.trentz@gmail.com>
2023-01-09 22:49:20 +0000

    comments

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-# Import uuid generator
 from uuid import uuid4
 import subprocess
 import os
 
-# Create out dir if not exists
+# Create hidden out dir if not exists in user home
 out = f"{os.path.expanduser('~')}/.bcp"
 
 if not os.path.exists(out):
     os.makedirs(out)
 
-# Execute nvim
 uuid = uuid4()
 script = f"{out}/{uuid}.sh"
 
-# Create script with a comment
+# Create script with a comment as indication of type
 with open(script, "w+") as f:
     f.write("#!/bin/bash")
     f.write("\n")
     f.write("# Execute bash terminal command to  ")
 
-# Open with cursor at the end of the file
+# Open file with cursor at the end of the file and insert mode
 subprocess.run(["nvim", "-c", "normal G$", "+startinsert", script])
 subprocess.run(["chmod", "+x", script])
 subprocess.run(["bash", script])
