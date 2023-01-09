d111ce79d82d138f3b9ff05bddf670fa01233b5b
mtrentz <mateus.trentz@gmail.com>
2023-01-09 22:43:33 +0000

    using user home as dir

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+#!/usr/bin/python3
 # Import uuid generator
 from uuid import uuid4
 import subprocess
+import os
+
+# Create out dir if not exists
+out = f"{os.path.expanduser('~')}/.bcp"
+
+if not os.path.exists(out):
+    os.makedirs(out)
 
 # Execute nvim
 uuid = uuid4()
-script = f"{uuid}.sh"
+script = f"{out}/{uuid}.sh"
 
 # Create script with a comment
-with open(script, "w") as f:
+with open(script, "w+") as f:
     f.write("#!/bin/bash")
     f.write("\n")
     f.write("# Execute bash terminal command to  ")
